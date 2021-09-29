e25c01768f64184af3c041e05564519dbd25d5a8
@karina21 <kaina@2amigos.us>
2021-09-29 15:23:26 +0200

    instalacion y primeras bases con react

---
+++ learning-react/src/componets/MyComponent.js
@@ -0,0 +1,12 @@
+import React, {Component}from 'react';
+
+class Mycomponent extends Component{
+
+    render(){
+        return(
+            <h1> Hello, I am Component </h1>
+        );
+    }
+}
+
+export default Mycomponent;
